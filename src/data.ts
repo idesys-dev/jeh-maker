926b1fc576b7e963e1033a8acb5b109e44e5bb21
Nathan Seva <thykof@protonmail.ch>
2019-06-30 12:03:04 +0200

    Split phase component from optimization calculation

---
+++ src/data.ts
@@ -0,0 +1,7 @@
+export default {
+  urssafBase: 39.04, // Base URSSAF
+  jeContrib: 0.30116, // Part JE : Total des taux des cotisations indexées sur l'assiette de cotisation
+  jepay: 0.042, // Part JE : Total des taux des cotisations indexées sur la rémunération brute
+  ConsultantContrib: 0.1605, // Part Etudiant : Total des taux des cotisations indexées sur l'assiette de cotisation
+  ConsultantPay: 0 // Part Etudiant : Total des taux des cotisations indexées sur la rémunération brute
+}
---
+++ src/services/optimizePhase.ts
@@ -0,0 +1,39 @@
+import PhaseObject from '../types'
+import round from '../utils'
+import contributionRates from '../data'
+
+export function optimizeByPrice (phase: PhaseObject) {
+  // optimize : maximize the jeh to 400 €
+  if (phase.jeh < phase.price) {
+    if (phase.price >= 400) {
+      phase.jeh = 400
+    } else {
+      phase.jeh = phase.price
+    }
+  }
+
+  // compute data
+  phase.nbJeh = phase.price / phase.jeh
+  phase.pay = round(phase.price - (phase.price * phase.margin / 100))
+  phase.urssafJE = round(phase.nbJeh * contributionRates.urssafBase * contributionRates.jeContrib +
+    phase.pay * contributionRates.jepay)
+  phase.marginJE = round((phase.price - phase.pay - phase.urssafJE) / phase.price * 100)
+  phase.urssafConsultant = round(phase.nbJeh * contributionRates.urssafBase * contributionRates.ConsultantContrib +
+    phase.pay * contributionRates.ConsultantPay)
+  phase.netConsultant = round(phase.pay - phase.urssafConsultant)
+  phase.netByConsultant = round(phase.netConsultant / phase.nbConsultant)
+  phase.pcConsultant = round(phase.netByConsultant / phase.price * 100)
+
+  // optimize : if the number of JEH is not integer,
+  // then we need to increase the number of JEH and drecrease the amount of each JEH.
+  if (!Number.isInteger(phase.nbJeh)) {
+    phase.nbJeh = Math.floor(phase.nbJeh) + 1
+    phase.jeh = phase.price / phase.nbJeh
+  }
+
+  return phase
+}
+
+export function optimizeByPay (phase: PhaseObject) {
+  return phase
+}
