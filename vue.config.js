1ce1dda6ea2253eba272f6a8765e58f0ee0c62f8
Nathan Seva <thykof@protonmail.ch>
2019-07-03 21:50:35 +0200

    Add vue config for gitlab pages

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js file to be place in the root of your repository
+// make sure you update `yourProjectName` with the name of your GitLab project
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  // ? '/yourProjectName/'
+    ? '/jeh-maker-v2/'
+    : '/'
+}
