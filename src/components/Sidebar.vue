4f93e5a0a05d7399bd208c7d8cb53d41a28c9c36
BilelJegham <contact.bileljegham@gmail.com>
2019-12-23 23:13:21 +0100

    Init Sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+<SuiSidebar animation="overlay" :visible="visible" direction="right">
+
+      <button @click="toogleSibebar"  class="btnmenu"><i class="icon close"/></button>
+      <h1 is="sui-header">Hello</h1>
+
+</SuiSidebar>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Sidebar extends Vue {
+  @Prop() visible!:boolean
+
+  toogleSibebar () {
+    this.$emit('toogle')
+  }
+}
+</script>
+<style scoped>
+.btnmenu{
+  padding: 14px;
+  left: 0;
+}
+</style>
