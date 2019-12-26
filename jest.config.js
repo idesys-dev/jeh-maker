2b7793846cb9cfdcd9c0cf9efe83597ca9b4d74b
BilelJegham <contact.bileljegham@gmail.com>
2019-12-27 00:51:47 +0100

    Fix Ci :coverage + linter

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!**/node_modules/**'],
+  collectCoverageFrom: ['src/**/*.{vue,js,jsx,ts,tsx}', '!**/node_modules/**'],
   coverageReporters: ['lcov']
 }
