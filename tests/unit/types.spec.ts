6f9e437f5b17b4adca721474dafcf1e3aff2c8d3
BilelJegham <contact.bileljegham@gmail.com>
2019-12-27 00:48:26 +0100

    Fix CodeCov Env car

--- tests/unit/types.spec.ts
+++ tests/unit/types.spec.ts
@@ -1,8 +1,8 @@
-import { TauxObject } from "@/types.ts"
+import { TauxObject } from '@/types.ts'
 
-describe("TauxObject", () => {
-    it("create TauxObject", () => {
-        const t = new TauxObject()
-        expect(t.urssafBase).toEqual(39.04)
-    });
-});
+describe('TauxObject', () => {
+  it('create TauxObject', () => {
+    const t = new TauxObject()
+    expect(t.urssafBase).toEqual(39.04)
+  })
+})
