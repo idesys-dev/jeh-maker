438231969e523f755da25bf7ae0d31d9221dfbb8
BilelJegham <contact.bileljegham@gmail.com>
2020-01-04 01:37:56 +0100

    FIx space test

--- tests/unit/filters.spec.ts
+++ tests/unit/filters.spec.ts
@@ -4,17 +4,17 @@ import { euro, percentage } from '@/plugins/filters/filters'
 describe('Filter euro', () => {
   it('1550.04', () => {
     const t = euro(1550.04)
-    expect(t).toBe('1 550,04 €')
+    expect(t).toBe('1 550,04 €')
   })
   it('1550.049', () => {
     const t = euro(1550.049)
-    expect(t).toBe ('1 550,05 €')
+    expect(t).toBe ('1 550,05 €')
   })
 })
 
 describe('Filter percentage', () => {
   it('50.04', () => {
-    const t = percentage(0.00504)
-    expect(t).toBe('50,04 %')
+    const t = percentage(50.04)
+    expect(t).toBe('50,04 %')
   })
 })
