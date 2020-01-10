13c63a9fd50816c2d240b2ee5cf470ba7b4154ba
BilelJegham <contact.bileljegham@gmail.com>
2020-01-10 22:30:32 +0100

    Filter Storybbok

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,11 @@
 import { configure } from '@storybook/vue';
 
 import 'semantic-ui-css/semantic.min.css'
-import '../src/plugins/filters'
 import Vue from 'vue'
 import SuiVue from 'semantic-ui-vue'
+import Filters from '../src/plugins/filters'
 
+Vue.use(Filters)
 Vue.use(SuiVue)
 
 
