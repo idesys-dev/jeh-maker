0218cb95b7f78398dcdc629bd96822145cc62b1f
Nathan Seva <thykof@protonmail.ch>
2020-02-09 15:18:03 +0100

    Update test

--- tests/unit/types.spec.ts
+++ tests/unit/types.spec.ts
@@ -3,6 +3,6 @@ import { TauxObject } from '@/types.ts'
 describe('TauxObject', () => {
   it('create TauxObject', () => {
     const t = new TauxObject()
-    expect(t.urssafBase).toEqual(39.04)
+    expect(t.urssafBase).toEqual(40.6)
   })
 })
