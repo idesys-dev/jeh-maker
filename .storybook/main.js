ff3d6212a985f7964b601255cb8c99748ac11154
Bilel Jegham <contact.bileljegham@gmail.com>
2021-10-01 18:24:07 +0000

    chore: migrate to stroybook 6 + add dependabot

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+   stories: ['../stories/*.stories.@(js)'],
+ 
+    addons: [
+        '@storybook/addon-actions',
+        '@storybook/preset-scss',
+        '@storybook/addon-links',
+        '@storybook/addon-knobs',
+      '@storybook/addon-storysource', 
+    ],
+  };
